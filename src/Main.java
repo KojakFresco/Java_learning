72739e4392ccdc94b00423989b603190ac347c75
KojakFresco <kirka1408kirka@gmail.com>
2022-11-20 17:11:37 +0300

    math

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+
+    private String name;
+    private static double y;
+    private static double S = 0;
+    private static double step;
+    private static double a;
+    private static double b;
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        a = 0;
+        b = 2;
+        step = 0.000001;
+        for (double x = a; x <= b; x = x + step) {
+            y = f(x);
+            S = S + y * step;
+        }
+        System.out.println(S);
+    }
+    private static double f(double x) {
+        return x * x;
     }
 }
